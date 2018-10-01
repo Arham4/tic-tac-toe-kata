40dd3894f902c08d24f8f57bcd1feb17ef8b2457
aryouham <arhamjsiddiqui@gmail.com>
2018-09-30 23:15:46 -0500

    more null check tests

--- src/test/java/com/gmail/arhamjsiddiqui/tictactoe/BoardTest.java
+++ src/test/java/com/gmail/arhamjsiddiqui/tictactoe/BoardTest.java
@@ -19,4 +19,16 @@ public class BoardTest {
         Session session = new Session();
         session.getBoard().mark(session.getPlayerOne(), null, Column.RIGHT);
     }
+
+    @Test(expected = NullPointerException.class)
+    public void placeMarkOnNullColumnShouldReturnNullPointerException() {
+        Session session = new Session();
+        session.getBoard().mark(session.getPlayerOne(), Row.TOP_ROW, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void placeMarkUsingNullPlayerShouldReturnNullPointerException() {
+        Session session = new Session();
+        session.getBoard().mark(null, Row.TOP_ROW, Column.LEFT);
+    }
 }
